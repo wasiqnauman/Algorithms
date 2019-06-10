7f987a60ca78ef0b3de770c2036fa76bad049374
wasiqnauman <wasiq_nauman@hotmail.com>
2019-06-10 16:18:49 +0500

    Added quickUnion for faster union

--- C++/UnionFind.cpp
+++ C++/UnionFind.cpp
@@ -27,13 +27,44 @@ public:
     }
 };
 
+class quickUnionUF
+{
+private:
+    static const int N = 1000;
+    int id[N];
+    
+    int root(int i)
+    {
+        while (i != id[i])
+            i = id[i];
+        return i;
+    }
+public:
+    quickUnionUF()
+    {
+        for (int i = 0; i < N; i++)
+            id[i] = i;
+    }
+    bool connected(int p, int q)
+    {
+        return root(p) == root(q);
+    }
+    void Union(int p, int q)
+    {
+        int i = root(p);
+        int j = root(q);
+        id[i] = j;
+    }
+
+};
+
 int main()
 {
-    quickFindUF UF;
+    quickUnionUF UF;
     UF.Union(4, 3);
     UF.Union(3, 8);
     UF.Union(6, 5);
     UF.Union(9, 4);
     UF.Union(2, 1);
-    cout << UF.connected(0, 7);
+    cout << UF.connected(8, 9);
 }
