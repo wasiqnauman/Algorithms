179e2141003b8bdb866763a8bcc0b172061284c0
wasiqnauman <wasiq_nauman@hotmail.com>
2019-06-10 14:56:06 +0500

    Added comments to code

--- C++/quickfind.cpp
+++ C++/quickfind.cpp
@@ -7,19 +7,19 @@ using namespace std;
 class quickFindUF
 {
 private:
-    static const int N = 1000;
-    int id[N];
+    static const int N = 1000; //length of array
+    int id[N];   //create new array of length N
 public:
-    quickFindUF()
+    quickFindUF()   //initialize array to values 0<=i<N
     {
         for (int i = 0; i < N; i++)
             id[i] = i;
     }
-    bool connected(int p, int q)
+    bool connected(int p, int q)   //if id[p] == id[q] they are connected
     {
         return id[p] == id[q];
     }
-    void Union(int p, int q)
+    void Union(int p, int q)   //change id[p] to id[q]
     {
         int pID = id[p];
         int qID = id[q];
