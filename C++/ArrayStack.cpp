a9fafbaa2a167ffea4b247c99e04e28bacce85cc
Wasiq <wasiq_nauman@hotmail.com>
2019-07-04 05:29:44 -0700

    Implementation of stacks using resizable arrays

--- C++/ArrayStack.cpp
+++ C++/ArrayStack.cpp
@@ -15,36 +15,40 @@ public:
         capacity = 1;
         s = new string[capacity];   //create initial array with capacity 1
     }
-    
-    void resize(int newCapacity)
-    {
-        string* copy = new string[newCapacity];   //create a new array with twice the capacity of the previous one
-        for (int i = 0; i < N; i++)   //copy the data from old to new array
-        {
-            copy[i] = s[i];
-        }
-        delete[] s;   //delete the old array to clear memory
-        s = copy;
-        capacity = newCapacity;
-    }
-    string pop()
-    {
-        string item = s[--N];
-        if (N > 0 && N == capacity / 4)   //shrink the array
-            resize(capacity / 2);
-        return item;
-    }
-    void push(string val)
-    {
-        if (N == capacity)
-            resize(capacity * 2);
-        s[N++] = val;
-    }
-    bool isEmpty()
-    {
-        return N == 0;
-    }
+
+    void resize(int newCapacity);
+    string pop();
+    void push(string val);
+    bool isEmpty();
 };
+void Stack::resize(int newCapacity)
+{
+    string* copy = new string[newCapacity];   //create a new array with twice the capacity of the previous one
+    for (int i = 0; i < N; i++)   //copy the data from old to new array
+    {
+        copy[i] = s[i];
+    }
+    delete[] s;   //delete the old array to clear memory
+    s = copy;
+    capacity = newCapacity;
+}
+string Stack::pop()
+{
+    string item = s[--N];
+    if (N > 0 && N == capacity / 4)   //shrink the array
+        resize(capacity / 2);
+    return item;
+}
+void Stack::push(string val)
+{
+    if (N == capacity)
+        resize(capacity * 2);
+    s[N++] = val;
+}
+bool Stack::isEmpty()
+{
+    return N == 0;
+}
 
 int main()  //test
 {
