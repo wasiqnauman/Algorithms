efa735f1627c47ba2513b0d9fe94d7f95946d7ba
Wasiq <wasiq_nauman@hotmail.com>
2019-07-10 14:43:54 -0700

    Implementing Queues using linked lists

---
+++ Python/Queue.py
@@ -0,0 +1,28 @@
+#Note: I am a C++ developer trying to learn Python, so my main focus is getting the code to work.
+class Node:
+    def __init__(self,item,next=None):
+        self.item = item
+        self.next = next
+class Queue:
+    def __init__(self):
+        self.first = None
+        self.last = None
+    def enqueue(self,item):
+        if self.first == None:
+            self.last = Node(item)
+            self.first = self.last
+        else:
+            self.last.next = Node(item)
+            self.last = self.last.next
+    def dequeue(self):
+        item = self.first.item
+        self.first = self.first.next
+        return item
+    def isEmpty(self):
+        return self.first == None
+
+#Test
+X = Queue()
+X.enqueue("Hello ")
+X.enqueue("World")
+print(X.dequeue() + X.dequeue())
